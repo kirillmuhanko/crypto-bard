d99d5b8eee469201f3fbb31d7acb3ac63ff20e4d
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2023-12-02 15:43:42 +0200

    chore: delta

--- CryptoBardWorkerService/Worker.cs
+++ CryptoBardWorkerService/Worker.cs
@@ -74,7 +74,7 @@ public class Worker : BackgroundService
             var lastPriceChangePercentage = _priceChangeRepository.GetLastPriceChangePercentage(model.Symbol);
             var percentDifference = model.PriceChangePercent - lastPriceChangePercentage;
 
-            if (model.PriceChangePercent >= _options.Value.PriceChangedPercent && percentDifference > 1m)
+            if (model.PriceChangePercent >= _options.Value.PriceChangedPercent && percentDifference > 4m)
             {
                 var message = $"{model.Symbol} has risen by {model.PriceChangePercent:F2}% in the last 24 hours!";
                 await _notificationService.Notify(message);
